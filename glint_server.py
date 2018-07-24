c93ced2e4aeb9ed89578a17597ca83872948cee0
Pete Wright <pewright77@gmail.com>
2018-07-24 14:19:59 -0400

    Remove set of problem words

--- glint_server.py
+++ glint_server.py
@@ -9,9 +9,7 @@ server = SimpleXMLRPCServer(('localhost',port))
 server.register_introspection_functions()
 
 # words that should not appear in an imperative sentence
-non_imperative_words = {'i', 'please', 'you', 'me'}
-# words handled poorly by spacy
-problem_words = {'rework', 'update', 'fix', 'terminate'}
+non_imperative_words = {'i', 'please', 'you'}
 
 def is_imperative(msg):
     '''
@@ -20,14 +18,11 @@ def is_imperative(msg):
     nlp = spacy.load('en_core_web_sm')
     out = nlp(msg)
 
-    #TODO find a more elegant solution for problem words
     if out[0].tag_ != 'VB':
-        text = out[0].text.lower()
-        if text not in problem_words:
-            print('%s is not a simple verb' % text)
-            return 0
+        print('%s is not a simple verb' % out[0].text)
+        return 0
 
-    # check if there is a subject
+    # check if there is a word that breaks imperative tense
     for word in out:
         if word.text.lower() in non_imperative_words:
             return 0
